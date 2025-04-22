0999496c84be03a10e7dae4c6a1cddfa84156b16
Matias <lit.io30303@gmail.com>
2025-04-22 13:41:21 -0400

    nav bar complete con framer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
+import Header from "@/components/Header/Header";
 
 export default function Home() {
   return (
-    <div>
-      Hola
-    </div>
+    <Header></Header>
   );
 }
